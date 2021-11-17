a3e99afaa1bfd8a12ac9ec8f02cb56e9afd37b32
Duong Van Chan <duongchan159@gmail.com>
2021-11-17 14:48:16 +0700

    Add sanitizing input on ajax controller

--- mvc/controllers/ajax.php
+++ mvc/controllers/ajax.php
@@ -11,24 +11,30 @@ class Ajax extends Controller {
     }
 
     public function regCheckUsername() {
+        // sanitize input
+        $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
         $username = $_POST["username"];
         echo $this->userModel->regCheckUsername($username);
     }
     public function regCheckPassword() {
+        $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
         $password = $_POST["password"];
         echo $this->userModel->regCheckPassword($password);
     }
     public function regCheckConfirmPassword() {
+        $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
         $password = $_POST["password"];
         $confirmPassword = $_POST["confirmPassword"];
         echo $this->userModel->regCheckConfirmPassword($password, $confirmPassword);
     }
     public function regCheckEmail() {
+        $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
         $email = $_POST["email"];
         echo $this->userModel->regCheckEmail($email);
     }
 
     public function contactCheckEmail() {
+        $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
         $email = $_POST["email"];
         echo $this->contactModel->contactCheckEmail($email);
     }

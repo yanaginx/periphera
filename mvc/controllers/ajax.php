b6ac5d096361fe741c0def53837f7d545f140918
Duong Van Chan <duongchan159@gmail.com>
2021-11-28 19:58:18 +0700

    Sanitize input for AJAX also

--- mvc/controllers/ajax.php
+++ mvc/controllers/ajax.php
@@ -11,25 +11,30 @@ class Ajax extends Controller {
     }
 
     public function regCheckUsername() {
-        $username = $_POST["username"];
+        $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+        $username = trim($_POST["username"]);
         echo $this->userModel->regCheckUsername($username);
     }
     public function regCheckPassword() {
-        $password = $_POST["password"];
+        $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+        $password = trim($_POST["password"]);
         echo $this->userModel->regCheckPassword($password);
     }
     public function regCheckConfirmPassword() {
-        $password = $_POST["password"];
-        $confirmPassword = $_POST["confirmPassword"];
+        $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+        $password = trim($_POST["password"]);
+        $confirmPassword = trim($_POST["confirmPassword"]);
         echo $this->userModel->regCheckConfirmPassword($password, $confirmPassword);
     }
     public function regCheckEmail() {
-        $email = $_POST["email"];
+        $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+        $email = trim($_POST["email"]);
         echo $this->userModel->regCheckEmail($email);
     }
 
     public function contactCheckEmail() {
-        $email = $_POST["email"];
+        $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+        $email = trim($_POST["email"]);
         echo $this->contactModel->contactCheckEmail($email);
     }
 
